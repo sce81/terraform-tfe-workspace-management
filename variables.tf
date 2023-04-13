3b7091ceb4bf8ada1769f2b44e1a03b07a4de170
Simon Elliott <simon.elliott@hashicorp.com>
2023-04-13 15:57:47 +0100

    develop - added support for HCL flag

--- variables.tf
+++ variables.tf
@@ -28,6 +28,7 @@ variable "tfe_variables" {
       value       = optional(string)
       description = optional(string)
       category    = optional(string)
+      hcl         = optional(string)
       sensitive   = optional(bool) // it is a general recommendation to not deploy sensitive variables using terraform 
   }))
   default = {}
