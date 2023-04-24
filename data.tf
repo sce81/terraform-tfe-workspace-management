fab998fe669792b42ee1a64f7f6f0588e931f748
Simon Elliott <simon.elliott@hashicorp.com>
2023-04-24 17:58:02 +0100

    develop - fixed auto_apply

--- data.tf
+++ data.tf
@@ -1,14 +1,3 @@
 locals {
-  auto_apply = contains(data.tfe_workspace.main.tag_names, "production") == true ? false : true
-}
-
-
-data "tfe_workspace" "main" {
-  name         = var.name
-  organization = var.organization
-
-  depends_on = [
-    tfe_workspace.main.name
-  ]
-
+      auto_apply = contains(var.workspace_tags, "production") == true ? false : true
 }
